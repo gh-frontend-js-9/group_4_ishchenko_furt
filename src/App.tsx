0fa51467ec77c15e05ac16a4ef21755108e4ac34
Karina1213 <karinaa121213@gmail.com>
2020-03-15 18:48:32 +0200

    add article card component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css'
 import {Header} from "./redux/components/Header/Header";
 import {Wrapper} from "./redux/components/Wrapper/Wrapper";
+import {ArticleCard} from "./redux/components/ArticleCard/ArticleCard";
 
 
 const App: React.FC = () => {
     return (
         <Wrapper>
             <Header/>
+            <ArticleCard/>
         </Wrapper>
     )
 };
---
+++ src/redux/components/ArticleCard/ArticleCard.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import {DesignProcessCard} from "../DesignProcessCard/DesignProcessCard";
+
+
+export const ArticleCard: React.FC = () => {
+    return (
+        <div className='article-card'>
+            <DesignProcessCard/>
+        </div>
+    );
+};

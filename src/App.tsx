383f05c97213b0284d0ad45b4b04aaad7537cbea
alex <appofeoz00@gmail.com>
2020-03-18 22:31:42 +0200

    working files,etc

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import {Header} from "./redux/components/Header/Header";
 import {Wrapper} from "./redux/components/Wrapper/Wrapper";
 import BlogPostPage from "./redux/pages/BlogPostPage";
 import MainCardPage from "./redux/pages/MainCardPage";
+import Slider from "./redux/components/Slider/Slider";
 
 const App: React.FC = () => {
     return (

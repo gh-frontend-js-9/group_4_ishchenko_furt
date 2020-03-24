b772c1d31f044c97334aa102c08dc7d1e06a4b47
alex <appofeoz00@gmail.com>
2020-03-24 20:35:44 +0200

    finished ListCard: responsive/small changes

--- src/redux/components/ListCard/ListCard.tsx
+++ src/redux/components/ListCard/ListCard.tsx
@@ -3,7 +3,7 @@ import './ListCard.scss'
 
 export const ListCard: React.FC = (props:any) => {
     return(
-        <div className={'list-container'}>
+        <div  className={'list-container list-container--hovered'}>
             <div className={'list-card'}/>
             <h3 className={'list-title'}>{props.listTitle}</h3>
         </div>

6b5957370b6420fc6739333cbf7529a7ef694530
alex <appofeoz00@gmail.com>
2020-04-13 20:55:03 +0300

    rewrote image component for post

--- src/redux/components/ImageBox/ImageBox.tsx
+++ src/redux/components/ImageBox/ImageBox.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import './ImageBox.scss'
 
 export const ImageBox: React.FC = (props: any) => {
+    const imageStyle = (props.imagePost ? 'featured-img--post-styles': 'featured-img')
+
     return <>
         <div  key={props._id}>
-            <img className="featured-img featured-img--hovered"  alt='nature' src= {(props.featuredImage)}/>
+            <img className={`${imageStyle} featured-img--hovered`}  alt='nature' src= {(props.featuredImage)}/>
         </div>
     </>
 };

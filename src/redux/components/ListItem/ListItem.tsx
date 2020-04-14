df720e4d59685a6b6fe2073dadf9a1116bb5f722
alex <appofeoz00@gmail.com>
2020-04-15 00:13:54 +0300

    styles for list item

--- src/redux/components/ListItem/ListItem.tsx
+++ src/redux/components/ListItem/ListItem.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import '../Content/Content.scss'
+import './ListItem.scss'
 
 export const ListItem: React.FC = (props: any) =>{
     const {children} = props
 
     return(
-        <li>
+        <li className={'content--md-font-size li-item'}>
             {children}
         </li>
     )

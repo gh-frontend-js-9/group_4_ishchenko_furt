3c6b1bc04b1e4424233752907fcf938dc533ee2d
alex <appofeoz00@gmail.com>
2020-04-15 00:02:10 +0300

    more flexible styles

--- src/redux/components/Content/Content.tsx
+++ src/redux/components/Content/Content.tsx
@@ -3,9 +3,10 @@ import React from 'react';
 export const Content = (props) => {
     const {children} = props;
 
+    const paddingSize = (props.postPadding ? 'content--post-pd' : '');
     const fontSizeContent = (props.mdFontSize ? "content--md-font-size" : 'content--lg-20px-font-size' );
     const colorContent = (props.greyColor ? "main-card__paragraph--grey" : '');
-    const classContent = ` ${fontSizeContent} ${colorContent}`;
+    const classContent = ` ${fontSizeContent} ${colorContent} ${paddingSize}`;
 
     return (
         <p className={classContent}>

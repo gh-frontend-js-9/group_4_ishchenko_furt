f178e724a4c0f2278f7f697e315dc8051f686c9a
alex <appofeoz00@gmail.com>
2020-04-17 16:55:15 +0300

    finished calendar

--- src/redux/components/Calendar/Calendar.tsx
+++ src/redux/components/Calendar/Calendar.tsx
@@ -1,43 +1,16 @@
 import React from "react";
 import DayPicker from "react-day-picker";
 import 'react-day-picker/lib/style.css';
-import './Calendar.scss'
+import './Calendar.scss';
+import  {WEEKDAYS_SHORT} from './CalendarSetting';
 
-
-
-/*const mapDayOfWeek: any = () =>{
-    /!*
-
-    const renderDayOfWeeks: any = daysOfWeek.map((day:any)=>(
-        <span>
-            {day}
-        </span>
-    ));*!/
-
-    return 'S M T W T F S'
-}
-
-const dayOfWeek :any = {
-   localeUnits: {
-       S:'S',
-       M:'M',
-       T:'T',
-       W:'W',
-       TH:'T',
-       F:'F',
-       SU:'S'
-   }
-}
-
-const weekdaysShort = {
-    en-gb: ["M", "T", "W", "T", "F", "S", "S"]
-};*/
 
 
 export const Calendar: React.FC = () =>{
     return(
         <DayPicker
             showOutsideDays={true}
+            weekdaysShort={WEEKDAYS_SHORT}
         />
     )
 }

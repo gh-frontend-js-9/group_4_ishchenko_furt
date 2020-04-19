4c75a630bca957d86ec3035d66f0f6729e2c4959
alex <appofeoz00@gmail.com>
2020-04-19 12:25:14 +0300

    export slider to sidebar

--- src/redux/components/AuthorSidebar/AuthorSidebar.tsx
+++ src/redux/components/AuthorSidebar/AuthorSidebar.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import './AuthorSidebar.scss'
 
-export const AuthorSidebar: React.FC = () => {
+export const AuthorSidebar: React.FC = (props: any) => {
+    const {children} = props;
 
     return (
         <div className='author-sidebar'>
-
+            {children}
         </div>
     )
 };
--- src/redux/pages/SubscribePage.tsx
+++ src/redux/pages/SubscribePage.tsx
@@ -3,6 +3,8 @@ import {Wrapper} from "../components/Wrapper/Wrapper";
 import {AuthorTheme} from "../components/AuthorTheme/AuthorTheme";
 import {AuthorSidebar} from "../components/AuthorSidebar/AuthorSidebar";
 import {Box} from "../components/Box/Box";
+import {AuthorSlider} from "../components/AuthorSlider/AuthorSlider";
+import {Calendar} from "../components/Calendar/Calendar";
 
 export const SubscribePage: React.FC = () => {
 

22e94db5d6a0849fb851e959e86fbfbce907c625
alex <appofeoz00@gmail.com>
2020-04-19 18:23:29 +0300

    small responsive fixes for title

--- src/redux/pages/NewPage.tsx
+++ src/redux/pages/NewPage.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {Wrapper} from "../components/Wrapper/Wrapper";
 import {Post} from "../components/Post/Post";
+import {TitlePostCard} from "../components/TitlePostCard/TitlePostCard";
 import {InsideWrapper} from "../components/InsideWrapper/InsideWrapper";
 import {CommentsBox} from "../components/CommentsBox/CommentsBox";
 
@@ -9,6 +10,8 @@ export const NewPage: React.FC = () => {
 
     return (
         <Wrapper>
+            <TitlePostCard/>
+
             <Post/>
 
             <InsideWrapper>

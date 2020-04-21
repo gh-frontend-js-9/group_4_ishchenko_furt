0f8f9eeb34f513e83afe30c2b702c7e7566de299
alex <appofeoz00@gmail.com>
2020-04-21 15:31:08 +0300

    sidebar posts

---
+++ src/redux/components/SidebarPopularPost/SidebarPopularPost.tsx
@@ -0,0 +1,56 @@
+import React, {Component} from "react";
+import {SidebarBox} from "../SidebarBox/SidebarBox";
+import './SidebarPost.scss';
+import {connect} from "react-redux";
+import {getPopularThematicPostAction} from '../../actions/getPostsActions';
+
+interface IProps {
+    getPopularThematicPostAction?: any,
+    isLoading?: boolean,
+    popularThematicPost?: any,
+}
+
+class SidebarPopularPost extends Component<IProps, {}>{
+    componentDidMount() {
+        this.props.getPopularThematicPostAction();
+    };
+
+    render(){
+        let renderPopularPost = this.props.popularThematicPost.map((post: any) => (
+            <div className={'sidebar-post'}>
+                <div className={'sidebar-post__image'} />
+                <div className={'sidebar-post__text'}>
+                    <p className={'sidebar-post__tag'}>
+                        {post.tags}
+                    </p>
+                    <h3 className={'sidebar-post__title'}>
+                        {post.title}
+                    </h3>
+                </div>
+            </div>
+        ));
+
+        return(
+            <SidebarBox>
+                <h3 className={'sidebar-title'}>
+                    Popular post
+                </h3>
+                {renderPopularPost}
+            </SidebarBox>
+        )
+    }
+}
+
+const mapStateToProps = (state: any) => {
+    return {
+        popularThematicPost: state.postsReducers.popularThematicPost,
+        isLoading: state.postsReducers.isLoading,
+    };
+};
+const mapDispatchToProps = (dispatch: any) => {
+    return {
+        getPopularThematicPostAction: () => dispatch(getPopularThematicPostAction()),
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SidebarPopularPost);

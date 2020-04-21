95b31a2e63c228e8ae2aef7c10c9fbac017a0845
Karina1213 <karinaa121213@gmail.com>
2020-04-21 13:02:09 +0200

    fix header and logo

--- src/redux/components/Navigation/Navigation.tsx
+++ src/redux/components/Navigation/Navigation.tsx
@@ -1,11 +1,12 @@
-import React from "react";
-import {NavLink} from "react-router-dom";
+import React from 'react';
+import {NavLink} from 'react-router-dom';
 import './Navigation.scss'
 
 export const Navigation = (props) => {
     const linkNavClass = (props.hoverElem
-        ? "navigation-bar__link navigation-bar__link--hovered"
+        ? 'navigation-bar__link navigation-bar__link--hovered'
         : 'navigation-bar__link');
+    const mixin = props.mixin;
 
     const navBar = [{
         link: '/',
@@ -23,10 +24,11 @@ export const Navigation = (props) => {
         link: '/subscribe',
         title: 'Subscribe'
     }];
+    const classes = `${linkNavClass} ${mixin}`;
 
     let renderNav = navBar.map((nav: any, n: number) => {
         return <span key={n} className="navigation-bar">
-                    <NavLink key={n} to={nav.link} className={linkNavClass}>
+                    <NavLink key={n} to={nav.link} className={classes}>
                         {nav.title}
                     </NavLink>
                 </span>
